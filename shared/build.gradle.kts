cdbb6e690eb5756b3a52cf3fddaae27689b07d8a
vshal9921 <vshal9921@gmail.com>
2026-01-25 19:48:28 +0530

    ktor - dependencies added

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,15 +26,18 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
         }
 
         androidMain.dependencies {
-
             implementation(libs.androidx.lifecycle.viewmodel.ktx)
+            implementation(libs.ktor.client.android)
         }
 
         iosMain.dependencies {
-
+            implementation(libs.ktor.client.darwin)
         }
 
         commonTest.dependencies {
